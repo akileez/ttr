919a8bfc3ee3aa0f04dfdbfde813f53b54b07f73
akileez <git@keeda.net>
2015-10-26 18:30:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ttr <https://github.com/akileez/ttr>
+ *
+ * Copyright © 2015 Keith Williams.
+ * Licensed under the ISC license.
+ */
+
+'use strict'
+
+module.exports = function () {
+  // do stuff
+}
